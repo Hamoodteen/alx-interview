2f23288e9a0aa92345e9d111c75eba251d3ba192
Hamoodteen <ahmed104567@outlook.com>
2024-04-24 19:44:09 +0200

    0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,7 +2,19 @@
 """
 commentttttttttttttttttttttttt
 """
-import math
+
+
+def factorial(n):
+    """ commentttttttttttttttttttttttt """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+def combination(n, r):
+    """ commentttttttttttttttttttttttt """
+    return factorial(n) // (factorial(r) * factorial(n - r))
 
 
 def pascal_triangle(n):
@@ -13,5 +25,5 @@ def pascal_triangle(n):
     for i in range(1, n + 1):
         ls.append([])
         for j in range(0, i):
-            ls[i - 1].append(math.comb(i - 1, j))
+            ls[i - 1].append(combination(i - 1, j))
     return ls
