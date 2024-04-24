dd2a9bcee55f266167ce8c87c3d88eb9e385f5a8
Hamoodteen <ahmed104567@outlook.com>
2024-04-24 19:37:37 +0200

    0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+commentttttttttttttttttttttttt
+"""
+import math
+
+
+def pascal_triangle(n):
+    ''' commentttttttttttttttttttttttt '''
+    ls = []
+    if n <= 0:
+        return []
+    for i in range(1, n + 1):
+        ls.append([])
+        for j in range(0, i):
+            ls[i - 1].append(math.comb(i - 1, j))
+    return ls
