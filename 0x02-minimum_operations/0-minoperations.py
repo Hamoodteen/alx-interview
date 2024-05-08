a0c2196f9b42aba36a234566a919c04eca913917
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2024-05-08 17:46:12 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
+"""
+commentttttttttttttttttttttttt
+"""
+
+
 def isqrt(n):
-        x=n
-        y=(x+n//x)//2
-        while(y<x):
-                x=y
-                y=(x+n//x)//2
-        return x
+    """ commentttttttttttttttttttttttt """
+    x=n
+    y=(x+n//x)//2
+    while(y<x):
+            x=y
+            y=(x+n//x)//2
+    return x
 def minOperations(n):
-        t0=isqrt(n)+1
-        counter=0
-        t=t0+counter
-        temp=isqrt((t*t)-n)
-        while((temp*temp)!=((t*t)-n)):
-                counter+=1
-                t=t0+counter
-                temp=isqrt((t*t)-n)
-        s=temp
-        p=t+s
-        q=t-s
-        return p + q
+    """ commentttttttttttttttttttttttt """
+    t0=isqrt(n)+1
+    counter=0
+    t=t0+counter
+    temp=isqrt((t*t)-n)
+    while((temp*temp)!=((t*t)-n)):
+            counter+=1
+            t=t0+counter
+            temp=isqrt((t*t)-n)
+    s=temp
+    p=t+s
+    q=t-s
+    return p + q
